2692120d8657c600f462bab8f58ea63adb0a7cd3
Didar Daniyarbekov <Didar.Daniyarbekov@gmail.com>
2022-11-18 19:29:17 +0600

    добавил вью multiply

--- core/app/urls.py
+++ core/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app.views import index, get_token, add
+from app.views import index, get_token, add, multiply
 
 urlpatterns = [
     path('', index, name="index"),
     path('token/', get_token, name='token'),
     path("add/", add, name="add" ),
+    path("multiply/", multiply, name="multiply" ),
 ]
--- core/app/views.py
+++ core/app/views.py
@@ -27,3 +27,16 @@ def add(request, *args, **kwargs):
             response = JsonResponse({"error": "Incorrect data"})
             response.status_code = 400
         return response
+
+
+def multiply(request, *args, **kwargs):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            a = float(data.get('A'))
+            b = float(data.get('B'))
+            response = JsonResponse({'answer': round(a * b, 1)})
+        except ValueError:
+            response = JsonResponse({"error": "Incorrect data"})
+            response.status_code = 400
+        return response
